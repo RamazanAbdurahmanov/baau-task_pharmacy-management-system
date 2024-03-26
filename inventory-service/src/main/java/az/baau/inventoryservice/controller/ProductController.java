d6c3b7e066d09c20f9a9b56ca6862a33d98983d1
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2024-03-27 00:36:17 +0400

    Hateoas added

--- inventory-service/src/main/java/az/baau/inventoryservice/controller/ProductController.java
+++ inventory-service/src/main/java/az/baau/inventoryservice/controller/ProductController.java
@@ -3,10 +3,13 @@ package az.baau.inventoryservice.controller;
 import az.baau.inventoryservice.dto.ProductDTO;
 import az.baau.inventoryservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -28,8 +31,13 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> getOneProduct(@PathVariable(name = "id") Long id) {
-        return new ResponseEntity<>(productService.getProductById(id), HttpStatus.FOUND);
+    public ResponseEntity<EntityModel<ProductDTO>>getOneProduct(@PathVariable(name = "id") Long id) {
+        ProductDTO productDTO = productService.getProductById(id);
+        //Burada Hateos istifade eledim . bu bir like sorgu atdiqda bagli olan diger metod linklerini de teklif kimi geri dondurur
+        EntityModel<ProductDTO> model=EntityModel.of(productDTO);
+        WebMvcLinkBuilder linkToProducts=linkTo(methodOn(this.getClass()).getALlProducts());
+        model.add(linkToProducts.withRel("all-products"));
+        return new ResponseEntity<>(model, HttpStatus.OK);
     }
 
     @PutMapping("/update/{id}")
